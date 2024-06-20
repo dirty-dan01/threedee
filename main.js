bd53f57f3a14fa50831a06cd8b10e529105dc5ed
dirty-dan01 <guilledaniel_1@hotmail.com>
2024-06-20 11:30:41 -0600

    Background Testing and Moon Maping

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { add } from 'three/examples/jsm/nodes/Nodes.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -33,6 +34,39 @@ const gridHelper = new THREE.GridHelper(200,50);
 scene.add(gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStars(){
+   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+   const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+   const star = new THREE.Mesh(geometry, material);
+
+   const [x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100))
+
+   star.position.set(x, y, z);
+   scene.add(star); 
+}
+
+Array(200).fill().forEach(addStars);
+
+const spaceTexture = new THREE.TextureLoader().load('ocean.jpg');
+scene.background = spaceTexture;
+
+const spaceCube = new THREE.TextureLoader().load('space.jpg');
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({map: spaceCube})
+);
+
+scene.add(cube)
+
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({map: moonTexture, normalMap: normalTexture})
+);
+scene.add(moon)
 
 function animate(){
   requestAnimationFrame(animate);
